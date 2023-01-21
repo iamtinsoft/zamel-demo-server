d04c57761aff88555b0fc03dfdc869fd49a8f7d3
Victor Fadipe <Viktinho56@gmail.com>
2023-01-21 06:57:11 +0100

    Eight Commit

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,6 @@
 const config = require("config");
 module.exports = function () {
-  if (!config.get("jwtPrivateKey")) {
+  if (!process.env.jwtPrivateKey) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
 };
