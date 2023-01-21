e15687aa6719264ba07bfda638a9228649d7a817
Victor Fadipe <Viktinho56@gmail.com>
2023-01-21 06:29:25 +0100

    Sixth Commit

--- startup/db.js
+++ startup/db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql2");
 const config = require("config");
 
 module.exports = function () {
-  const db_host = config.get("db_host");
-  const db_user = config.get("db_user");
-  const db_password = config.get("db_password");
-  const db_database = config.get("db_database");
+  const db_host = process.env.db_host;
+  const db_user = process.env.db_user;
+  const db_password = process.env.db_password;
+  const db_database = process.env.db_database;
 
   let connection = mysql.createConnection({
     host: db_host,
